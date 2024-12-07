43ea82dac2c5da8a23faee87ed9963339e93f9d5
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2024-12-07 18:31:44 +0100

    Day_7

---
+++ Day_7/Equasion.cs
@@ -0,0 +1,75 @@
+﻿namespace Day_7;
+
+public enum Operations
+{
+    Add = 0,
+    Multiply = 1,
+    Concat = 2
+}
+
+public class Equasion
+{
+    private readonly ulong _result;
+    private readonly Queue<ulong> _numbers = new Queue<ulong>();
+    private readonly Queue<Operations> _operationQueue = new Queue<Operations>();
+    private ulong _intermediateResult;
+    public Equasion(ulong result, IEnumerable<ulong> numbers)
+    {
+        _result = result;
+        foreach (ulong number in numbers)
+        {
+            _numbers.Enqueue(number);
+        }
+    }
+    public Equasion(ulong result, Queue<ulong> numbers)
+    {
+        _result = result;
+        _numbers = numbers;
+    }
+    private static readonly List<Operations> PossibleOperationsPart1 = [Operations.Add, Operations.Multiply];
+    private static readonly List<Operations> PossibleOperationsPart2 = [Operations.Add, Operations.Multiply, Operations.Concat];
+    public ulong Result => _result;
+
+    public bool SolveAll(bool part2)
+    {
+        var operationCount = _numbers.Count - 1;
+        var possibleOperationsForPart = part2 ? PossibleOperationsPart2 : PossibleOperationsPart1;
+        var possibleOperationPermutations = (int)Math.Pow(possibleOperationsForPart.Count, operationCount);
+
+        for (var i = 0; i < possibleOperationPermutations; i++)
+        {
+            var currentEquation = new Equasion(_result, new Queue<ulong>(_numbers));
+            var tempIndex = i;
+
+            for (int op = 0; op < operationCount; op++)
+            {
+                var operationIndex = tempIndex % possibleOperationsForPart.Count;
+                var operation = possibleOperationsForPart[operationIndex];
+                currentEquation._operationQueue.Enqueue(operation);
+
+                tempIndex /= possibleOperationsForPart.Count;
+            }
+
+            if (currentEquation.Solve())
+                return true;
+        }
+        return false;
+    }
+
+    private bool Solve()
+    {
+        _intermediateResult = _numbers.Dequeue();
+        foreach (var operationse in _operationQueue)
+        {
+            var num2 = _numbers.Dequeue();
+            _intermediateResult = operationse switch
+            {
+                Operations.Add => _intermediateResult + num2,
+                Operations.Multiply => _intermediateResult * num2,
+                Operations.Concat => _intermediateResult * (ulong)Math.Pow(10, (int)Math.Floor(Math.Log10(num2)) + 1) + num2,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        return _result == _intermediateResult;
+    }
+}
---
+++ Day_7/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Day_7;
+
+class Program
+{
+    private static async Task<string> ReadInputAsync()
+    {
+        using var reader = new StreamReader("input");
+        return await reader.ReadToEndAsync();
+    }
+    private static string DebugInput =
+        """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+        """;
+
+    private static IEnumerable<Equasion> GetEquasionsFromInput(string input)
+    {
+        foreach (string se in input.Trim('\r')
+                                   .Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+        {
+            var line = se.Trim('\r');
+            var splitLine = line.Split(':');
+            var result = ulong.Parse(splitLine[0]);
+            var numbers = splitLine[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            yield return new Equasion(result, GetNumbersFromSplitInput(numbers));
+        }
+    }
+
+    private static IEnumerable<ulong> GetNumbersFromSplitInput(string[] input)
+    {
+        foreach (string number in input)
+            yield return ulong.Parse(number);
+    }
+
+    private static IEnumerable<Task<ulong>> StartCalculations(Equasion[] equasions, bool part2)
+    {
+        foreach (var equasion in equasions)
+            yield return Task.Run(() => equasion.SolveAll(part2) ? equasion.Result : 0ul);
+    }
+
+    static async Task Main(string[] args)
+    {
+        var input = GetEquasionsFromInput(
+#if DEBUG
+            DebugInput
+#else
+            await ReadInputAsync()
+#endif
+        ).ToArray();
+
+        ulong value = 0;
+        foreach (ulong @ulong in await Task.WhenAll(StartCalculations(input, false)))
+        {
+            value += @ulong;
+        }
+        Console.WriteLine(value);
+        value = 0;
+        foreach (ulong @ulong in await Task.WhenAll(StartCalculations(input, true)))
+        {
+            value += @ulong;
+        }
+        Console.WriteLine(value);
+    }
+}
