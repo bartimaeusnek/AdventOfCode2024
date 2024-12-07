f1c643168e4cd7788fd974036a9c4a8b3696f984
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2024-12-07 02:24:03 +0100

    Day 6 finally.

---
+++ Day_6/Day_6.2/Program.cs
@@ -0,0 +1,163 @@
+﻿namespace Day_6._2;
+
+class Program
+{
+    private static readonly string debugInput =
+        """
+        ....#.....
+        .........#
+        ..........
+        ..#.......
+        .......#..
+        ..........
+        .#..^.....
+        ........#.
+        #.........
+        ......#...
+        """;
+    private static async Task<string> ReadInputAsync()
+    {
+        using var reader = new StreamReader("input");
+        return await reader.ReadToEndAsync();
+    }
+    
+    static async Task Main(string[] args)
+    {
+        var input = await ReadInputAsync();
+        var board = BuildLists(input);
+        var solved = Solve(board, out _);
+        Console.WriteLine(solved.Count);
+        var modifiedBoards = BuildModifiedLists(board, solved).ToArray();
+
+        var solutions = 0;
+        Parallel.ForEach(modifiedBoards, board =>
+        {
+            Solve(board, out var isInfinite);
+            if (isInfinite)
+                Interlocked.Increment(ref solutions);
+        });
+        Console.WriteLine(solutions);
+    }
+    
+    private static IEnumerable<Tile[][]> BuildModifiedLists(Tile[][] board, HashSet<(Tile, Direction)> path)
+    {
+        var flattened = board.SelectMany(x => x).ToArray();
+        var maxX = flattened.Max(x => x.X);
+        var maxY = flattened.Max(x => x.Y);
+        for (int y = 0; y <= maxY; y++)
+        {
+            for (int x = 0; x <= maxX; x++)
+            {
+                if (!path.Any(z => z.Item1.X == x && z.Item1.Y == y))
+                    continue;
+                var ret = new Tile[maxY+1][];
+                for (int cy = 0; cy <= maxY; cy++)
+                {
+                    ret[cy] = new Tile[maxX+1];
+                    for (int cx = 0; cx <= maxX; cx++)
+                    {
+                        if (board[cy][cx].IsStartingPoint)
+                        {
+                            ret[cy][cx] = new Tile(board[cy][cx].X, board[cy][cx].Y, false, true);
+                        }
+                        else if (cy == y && cx == x)
+                        {
+                            ret[cy][cx] = new Tile(board[cy][cx].X, board[cy][cx].Y, true, false);
+                        }
+                        else
+                        {
+                            ret[cy][cx] = new Tile(board[cy][cx].X, board[cy][cx].Y, board[cy][cx].IsWall, false);
+                        }
+                      
+                    }
+                }
+                yield return ret;
+            }
+        }
+    }
+    
+    
+    private record struct Tile(int X, int Y, bool IsWall, bool IsStartingPoint);
+    private static Tile[][] BuildLists(string readInput)
+    {
+        var lines = readInput.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var ret = new Tile[lines.Length][];
+        for (int index = 0; index < lines.Length; index++)
+        {
+            string line = lines[index];
+            ret[index] = new Tile[line.Length];
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+                ret[index][i] = new Tile(i, index, c == '#', c == '^');
+            }
+        }
+        return ret;
+    }
+
+    enum Direction
+    {
+        North,
+        South,
+        East,
+        West,
+    }
+    
+    private static HashSet<(Tile, Direction)> Solve(Tile[][] board, out bool isInfinite)
+    {
+        var flattened = board.SelectMany(x => x).ToArray();
+        var startingPoint = flattened.First(x => x.IsStartingPoint);
+        var maxX = flattened.Max(x => x.X);
+        var maxY = flattened.Max(x => x.Y);
+        var visited = new HashSet<(Tile, Direction)>();
+        var currentDirection = Direction.North;
+        Tile? currentTile = startingPoint;
+        visited.Add((startingPoint, currentDirection));
+        while (currentTile != null)
+        {
+            start:
+            (int X, int Y) nextCoord = currentDirection switch
+            {
+                Direction.North => (currentTile.Value.X, currentTile.Value.Y - 1),
+                Direction.South => (currentTile.Value.X, currentTile.Value.Y + 1),
+                Direction.East => (currentTile.Value.X + 1, currentTile.Value.Y),
+                Direction.West => (currentTile.Value.X - 1, currentTile.Value.Y),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            if (nextCoord.Y >= 0 && nextCoord.Y <= maxY)
+            {
+                if (nextCoord.X >= 0 && nextCoord.X <= maxX)
+                {
+                    var nextTile = board[nextCoord.Y][nextCoord.X];
+                    if (nextTile.IsWall)
+                    {
+                        currentDirection = currentDirection switch
+                        {
+                            Direction.North => Direction.East,
+                            Direction.South => Direction.West,
+                            Direction.East => Direction.South,
+                            Direction.West => Direction.North,
+                            _ => throw new ArgumentOutOfRangeException()
+                        };
+                        goto start;
+                    }
+                    currentTile = nextTile;
+                    if (!visited.Add((currentTile.Value, currentDirection)))
+                    {
+                        isInfinite = true;
+                        return visited;
+                    }
+                }
+                else
+                    currentTile = null;
+            }
+            else
+                currentTile = null;
+        }
+        isInfinite = false;
+        return visited;
+    }
+
+  
+}
---
+++ Day_6/Program.fs
@@ -0,0 +1,183 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+
+type Direction =
+    | North = 0
+    | East = 1
+    | South = 2
+    | West = 3
+    
+and Tile(isWall: bool, x: int, y: int, isStartingPoint: bool) =
+    member val IsWall = isWall with get
+    member val IsStartingPoint = isStartingPoint with get
+    member val X = x with get
+    member val Y = y with get
+
+and GameUnit(x: int, y: int, startingDirection:Direction, tiles:Tile seq) =
+    let mutable currentDirection = startingDirection
+    let mutable X = x 
+    let mutable Y = y
+    member val IsInfinite = false with get,set
+    member val Tiles = tiles
+    member val TileMap : IDictionary<int*int, Tile> = tiles |> Seq.map (fun t -> (t.X, t.Y), t) |> dict
+    
+    member this.Turn() =
+        currentDirection <-
+            match currentDirection with
+                | Direction.North -> Direction.East
+                | Direction.East -> Direction.South
+                | Direction.South -> Direction.West
+                | Direction.West -> Direction.North
+                | _ -> ArgumentOutOfRangeException() |> raise
+            
+    member inline this.GetTile(x, y) =
+      let mutable tile = Unchecked.defaultof<Tile>
+      if this.TileMap.TryGetValue((x, y), &tile) then
+            Some(tile)
+       else None
+            
+    member this.Peek()  =
+       let x,y = match currentDirection with
+                     | Direction.North -> X, Y-1
+                     | Direction.East -> X+1, Y
+                     | Direction.South -> X, Y+1
+                     | Direction.West -> X-1, Y
+                     | _ -> ArgumentOutOfRangeException() |> raise
+       this.GetTile(x,y)
+
+        
+    member inline this.CanMove() =
+        this.Peek().IsSome
+    
+    member this.Move() =
+        match currentDirection with
+            | Direction.North -> Y <- Y-1
+            | Direction.East -> X <- X+1
+            | Direction.South -> Y <- Y+1
+            | Direction.West -> X <- X-1
+            | _ -> ArgumentOutOfRangeException() |> raise
+     
+    member this.SolveInternal(hs : HashSet<int * int * Direction>) =
+        seq {
+                   if hs.Add(X, Y, currentDirection) then
+                       let thisTile = this.GetTile(X, Y)
+                       if thisTile.IsSome then
+                           yield Tile(thisTile.Value.IsWall, thisTile.Value.X, thisTile.Value.Y, thisTile.Value.IsStartingPoint)
+                       if this.CanMove() then
+                           this.Move()
+#if DEBUG
+                           // Console.Clear()
+                           // this.PrintBoard(hs)
+                           // Console.ReadKey() |> ignore
+#endif
+                       let nextTile = this.Peek()
+                       if nextTile.IsSome then
+                           if nextTile.Value.IsWall then
+                               this.Turn()
+                           yield! this.SolveInternal(hs)
+                   else
+                      this.IsInfinite <- true
+        }
+       
+    member this.PrintBoard(hs : HashSet<int*int*Direction>) =
+       this.Tiles |> Seq.iter(fun i ->
+           if i.X = 0 then
+               Console.WriteLine()
+               
+           if (hs |> Seq.tryFind(fun a ->
+               let x,y,_ = a
+               x = i.X && y = i.Y)).IsSome && not(X = i.X && Y = i.Y) then
+               Console.Write 'X'
+           elif i.IsWall then
+               Console.Write '#'
+           elif X = i.X && Y = i.Y then
+               Console.Write (match currentDirection with
+                                | Direction.North -> '^'
+                                | Direction.South -> 'v'
+                                | Direction.East -> '>'
+                                | Direction.West -> '<'
+                                | _ -> ArgumentOutOfRangeException() |> raise)
+           else
+               Console.Write '.'
+           )
+       
+    member inline this.GetPath() =
+         let hs = HashSet<int*int*Direction>()
+         this.SolveInternal(hs)
+         
+    member inline this.Solve() =
+        this.GetPath() |> Seq.distinct |> Seq.length
+        
+let debugInput =
+    """
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+    """
+    
+let getFileContent fileName =
+    use sr = File.OpenText(fileName)
+    sr.ReadToEnd()
+
+let buildLists (fileContent: string) =
+    fileContent.Split("\n", StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries)
+        |> Seq.mapi(fun y chars ->
+           chars |> Seq.mapi(fun x char ->
+               let isWall = char = '#'
+               let isStartingPoint = char = '^'
+               Tile(isWall, x, y, isStartingPoint))
+        )
+        |> Seq.collect id
+
+#if DEBUG
+let input = buildLists(debugInput)
+#else
+let input = buildLists(getFileContent("input"))
+#endif
+let startingPoint = input |> Seq.find(_.IsStartingPoint)
+let unit = GameUnit(startingPoint.X, startingPoint.Y, Direction.North, input)
+Console.WriteLine(unit.Solve()+1)
+
+#if DEBUG
+let unitP2 = GameUnit(startingPoint.X, startingPoint.Y, Direction.North, input)
+let path = unitP2.GetPath() |> Seq.distinct
+
+let part2 = path |> Seq.map(fun i ->
+                            input
+                            |> Seq.map (fun j ->
+                                if not i.IsStartingPoint && not i.IsWall then
+                                    Some(Tile((j.X = i.X && j.Y = i.Y) || j.IsWall, j.X, j.Y, j.IsStartingPoint))
+                                else
+                                    None
+                            )
+                            |> Seq.filter(_.IsSome)
+                            |> Seq.map(_.Value)
+                            )
+            |> Seq.distinct
+let tasks = part2 |> Seq.map(fun bruteForce -> async {
+       let nuUnit = GameUnit(startingPoint.X, startingPoint.Y, Direction.North, bruteForce)
+       nuUnit.Solve() |> ignore
+       return nuUnit.IsInfinite
+    }
+)
+
+let part2Length = tasks 
+                  #if DEBUG
+                  |> Async.Sequential
+                  #else
+                  |> Async.Parallel
+                  #endif
+                  |> Async.RunSynchronously
+                  |> Seq.filter(fun i -> i)
+                  |> Seq.length
+
+Console.WriteLine(part2Length)
+#endif
