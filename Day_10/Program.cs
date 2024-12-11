fcdd3668127afb7c806dfeb385452871dd57649b
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2024-12-12 00:56:27 +0100

    Day_10 & Day_11

---
+++ Day_10/Program.cs
@@ -0,0 +1,195 @@
+﻿namespace Day_10;
+
+class Program
+{
+    private const string SimpleDebugInput = """
+                                      ...0...
+                                      ...1...
+                                      ...2...
+                                      6543456
+                                      7.....7
+                                      8.....8
+                                      9.....9
+                                      """;
+
+    private const string DebugInput = """
+                                      89010123
+                                      78121874
+                                      87430965
+                                      96549874
+                                      45678903
+                                      32019012
+                                      01329801
+                                      10456732
+                                      """;
+
+    public static void ParseInput(string input)
+    {
+        var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var ret = new Cell[lines.Length][];
+        for (int y = 0; y < lines.Length; y++)
+        {
+            ret[y] = new Cell[lines[y].Length];
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                if (lines[y][x] == '.')
+                {
+                    ret[y][x] = new Cell(-1)
+                    {
+                        X = x,
+                        Y = y
+                    };
+                    continue;
+                }
+                ret[y][x] = new Cell(lines[y][x] - '0')
+                {
+                    X = x,
+                    Y = y
+                };
+            }
+        }
+        Cell.Field = ret;
+    }
+    
+    public static void PrintField(Cell[][] field)
+    {
+        for (var y = 0; y < field.Length; y++)
+        {
+            for (int x = 0; x < field[y].Length; x++)
+            {
+                Console.Write(field[y][x].Value);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    
+    private static async Task<string> ReadInputAsync()
+    {
+        using var reader = new StreamReader("input");
+        return await reader.ReadToEndAsync();
+    }
+    
+    static async Task Main(string[] args)
+    {
+        ParseInput(await ReadInputAsync());
+        
+        var fieldStartPointsFlattened = Cell.Field.SelectMany(x => x)
+                                             .Where(x => x.Value == 0)
+                                             .ToArray();
+
+        ulong totalScore = 0;
+        ulong totalRating = 0;
+        Parallel.ForEach(fieldStartPointsFlattened, cell =>
+        {
+            var node = new AoCNode(cell.Value, cell.X, cell.Y);
+            node.CheckNeighbours();
+            Interlocked.Add(ref totalScore, (ulong) node.GetScore());
+            Interlocked.Add(ref totalRating, (ulong) node.GetRating());
+        });
+        
+        PrintField(Cell.Field);
+        Console.WriteLine(totalScore);
+        Console.WriteLine(totalRating);
+        
+        Thread.Sleep(-1);
+    }
+}
+public class Cell(int value)
+{
+    public static Cell[][] Field = null!;
+    public int X { get; set; }
+    public int Y { get; set; }
+    public readonly int Value = value;
+}
+
+
+public class AoCNode(int value, int x, int y) : IEquatable<AoCNode>
+{
+    public bool Equals(AoCNode? other)
+    {
+        if (other is null)
+            return false;
+        if (ReferenceEquals(this, other))
+            return true;
+        return Value == other.Value && X == other.X && Y == other.Y;
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        if (obj.GetType() != GetType())
+            return false;
+        return Equals((AoCNode)obj);
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value, X, Y);
+    }
+    public static bool operator ==(AoCNode? left, AoCNode? right)
+    {
+        return Equals(left, right);
+    }
+    public static bool operator !=(AoCNode? left, AoCNode? right)
+    {
+        return !Equals(left, right);
+    }
+    
+    public readonly int Value = value;
+    public readonly int X = x;
+    public readonly int Y = y;
+    public readonly List<AoCNode> Children = new List<AoCNode>();
+
+    //Part2
+    public int GetRating()
+    {
+        return Children.FirstOrDefault() is { Value: 9 } ? Children.Count : Children.Sum(aoCNode => aoCNode.GetRating());
+    }
+
+    //Part1
+    public int GetScore()
+    {
+        return GetFlattenedChildren(Children).Where(x => x.Value == 9).Distinct().Count();
+    }
+    
+    public static IEnumerable<AoCNode> GetFlattenedChildren(IEnumerable<AoCNode> input)
+    {
+        if (input.Any(x => x.Value == 9))
+        {
+            return input;
+        }
+        return input.SelectMany(x => GetFlattenedChildren(x.Children));
+    }
+    
+    public void CheckNeighbours()
+    {
+        for (int y = -1; y <= 1; y++)
+        {
+            var dy = y + Y;
+            if (dy >= Cell.Field.Length  || dy < 0)
+            {
+                continue;
+            }
+            for (int x = -1; x <= 1; x++)
+            {
+                if (Math.Abs(x) == Math.Abs(y))
+                    continue;
+                
+                var dx = x + X;
+                if (dx >= Cell.Field[dy].Length || dx < 0)
+                {
+                    continue;
+                }
+                var neighbour = Cell.Field[dy][dx];
+                if (neighbour.Value != Value + 1)
+                    continue;
+                
+                var nuChild = new AoCNode(neighbour.Value, dx, dy);
+                Children.Add(nuChild);
+                nuChild.CheckNeighbours();
+            }
+        }
+    }
+}
---
+++ Day_11/Program.cs
@@ -0,0 +1,175 @@
+﻿namespace Day_11;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    public static List<Stone> ParseInput(string input) => input.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                                                                .Select(x => new Stone(ulong.Parse(x)))
+                                                                .ToList(); 
+    private static async Task<string> ReadInputAsync()
+    {
+        using var reader = new StreamReader("input");
+        return await reader.ReadToEndAsync();
+    }
+    
+    static async Task Main(string[] args)
+    {
+        Stone.Stones = new Dictionary<Stone, ulong>();
+        
+        foreach (var stone in ParseInput(await ReadInputAsync()))
+        {
+            Stone.Stones.Add(stone, 1ul);
+        }
+        ulong sum;
+        for (int i = 0; i < 75; i++)
+        {
+            var arr = Stone.Stones.ToArray();
+            Stone.Stones.Clear();
+            for (int index = 0; index < arr.Length; index++)
+            {
+                var stonesKey = arr[index];
+                stonesKey.Key.Blink(stonesKey.Value);
+            }
+            if (i == 24)
+            {
+                sum = Stone.Stones.Aggregate<KeyValuePair<Stone, ulong>, ulong>(0, (current, x) => current + x.Value);
+                Console.WriteLine("Part 1: " + sum);
+            }
+        }
+        sum = Stone.Stones.Aggregate<KeyValuePair<Stone, ulong>, ulong>(0, (current, x) => current + x.Value);
+        Console.WriteLine("Part 2: " + sum);
+    }
+
+    public class Stone(ulong value) : IEquatable<Stone>
+    {
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+        public static Dictionary<Stone, ulong> Stones;
+        private ulong _value = value;
+        public ulong Value => _value;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+        private static readonly ulong[] PowersOf10 =
+        [
+            1ul, 10ul, 100ul, 1000ul, 10000ul, 100000ul, 1000000ul, 10000000ul,
+            100000000ul, 1000000000ul, 10000000000ul, 100000000000ul, 1000000000000ul,
+            10000000000000ul, 100000000000000ul, 1000000000000000ul, 10000000000000000ul,
+            100000000000000000ul, 1000000000000000000ul, 10000000000000000000ul
+        ];
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ulong Pow10(int value)
+        {
+            return PowersOf10[value];
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Log10(ulong i)
+        {
+            return (int)Lzcnt.X64.LeadingZeroCount(i) switch
+            {
+                64 or 63 or 62 or 61 => 0,
+                60 => i < 10 ? 0 : 1,
+                59 or 58 => 1,
+                57 => i < 100 ? 1 : 2,
+                56 or 55 => 2,
+                54 => i < 1000 ? 2 : 3,
+                53 or 52 or 51 => 3,
+                50 => i < 10000 ? 3 : 4,
+                49 or 48 => 4,
+                47 => i < 100000 ? 4 : 5,
+                46 or 45 => 5,
+                44 => i < 1000000 ? 5 : 6,
+                43 or 42 or 41 => 6,
+                40 => i < 10000000 ? 6 : 7,
+                39 or 38 => 7,
+                37 => i < 100000000 ? 7 : 8,
+                36 or 35 => 8,
+                34 => i < 1000000000 ? 8 : 9,
+                33 or 32 or 31 => 9,
+                30 => i < 10000000000 ? 9 : 10,
+                29 or 28 => 10,
+                27 => i < 100000000000 ? 10 : 11,
+                26 or 25 => 11,
+                24 => i < 1000000000000 ? 11 : 12,
+                23 or 22 or 21 => 12,
+                20 => i < 10000000000000 ? 12 : 13,
+                19 or 18 => 13,
+                17 => i < 100000000000000 ? 13 : 14,
+                16 or 15 => 14,
+                14 => i < 1000000000000000 ? 14 : 15,
+                13 or 12 or 11 => 15,
+                10 => i < 10000000000000000 ? 15 : 16,
+                9 or 8 => 16,
+                7 => i < 100000000000000000 ? 16 : 17,
+                6 or 5 => 17,
+                4 => i < 1000000000000000000 ? 17 : 18,
+                3 or 2 or 1 => 18,
+                0 => i < 10000000000000000000UL ? 18 : 19,
+                _ => throw new ArgumentException("Unexpected number of leading zeros")
+            };
+        }
+
+        public void Blink(ulong count)
+        {
+            var digitsCheck = Log10(_value);
+            Stone newStone;
+            if (_value == 0)
+            {
+                newStone = new Stone(1);
+            }
+            else if (IsEven(digitsCheck + 1))
+            {
+                int digits = digitsCheck / 2 + 1;
+                ulong divisor = Pow10(digits);
+        
+                ulong firstHalf = _value / divisor;
+                ulong remainder = _value % divisor;
+
+                newStone = new Stone(firstHalf);
+                if (!Stones.TryAdd(newStone, count))
+                {
+                    Stones[newStone] = Stones[newStone] += count;
+                }
+                newStone = new Stone(remainder);
+            }
+            else
+            {
+                newStone = new Stone(_value * 2024ul);
+            }
+            if (!Stones.TryAdd(newStone, count))
+            {
+                Stones[newStone] = Stones[newStone] += count;
+            }
+        }
+        
+        public bool Equals(Stone other)
+        {
+            return _value == other._value;
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is Stone other && Equals(other);
+        }
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+        public static bool operator ==(Stone left, Stone right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(Stone left, Stone right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
